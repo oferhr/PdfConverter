9ec7724cb7049a57b6126f1b3045b8cf7545f59a
Ofer Harari <offa23@hotmail.com>
2020-06-26 19:28:29 +0000

    Version 1.0.0
    git-tfs-id: [https://oferh.visualstudio.com/DefaultCollection]$/Projects/PdfConverter;C118

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace PdfConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblPb = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDetails = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(859, 170);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 28);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "התחל";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(859, 36);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(100, 28);
+            this.btnBrowse.TabIndex = 7;
+            this.btnBrowse.Text = "בחר";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1000, 42);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "בחר תיקייה";
+            // 
+            // txtDir
+            // 
+            this.txtDir.Location = new System.Drawing.Point(134, 39);
+            this.txtDir.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(686, 22);
+            this.txtDir.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(134, 111);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(825, 23);
+            this.progressBar1.TabIndex = 9;
+            this.progressBar1.Visible = false;
+            // 
+            // lblPb
+            // 
+            this.lblPb.AutoSize = true;
+            this.lblPb.Location = new System.Drawing.Point(131, 82);
+            this.lblPb.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPb.Name = "lblPb";
+            this.lblPb.Size = new System.Drawing.Size(0, 16);
+            this.lblPb.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(859, 433);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "סגור";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtDetails
+            // 
+            this.txtDetails.BackColor = System.Drawing.Color.Silver;
+            this.txtDetails.Location = new System.Drawing.Point(35, 219);
+            this.txtDetails.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDetails.Name = "txtDetails";
+            this.txtDetails.ReadOnly = true;
+            this.txtDetails.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtDetails.Size = new System.Drawing.Size(1025, 189);
+            this.txtDetails.TabIndex = 12;
+            this.txtDetails.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 465);
+            this.Controls.Add(this.txtDetails);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblPb);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblPb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox txtDetails;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.Office.Interop.Word;
+using SimpleLogger;
+using Spire.Pdf;
+using Spire.Pdf.Graphics;
+using Spire.Pdf.HtmlConverter;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PdfConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var dirPath = Properties.Settings.Default.DirPath;
+            if (!string.IsNullOrEmpty(dirPath))
+            {
+                txtDir.Text = dirPath;
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    Properties.Settings.Default.DirPath = fbd.SelectedPath;
+                    Properties.Settings.Default.Save();
+                    txtDir.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrEmpty(txtDir.Text))
+            {
+                MessageBox.Show("יש לבחור תיקייה");
+                return;
+            }
+            SimpleLog.SetLogFile(".\\Log", "Log_");
+            progressBar1.Visible = true;
+            progressBar1.Value = 0;
+            string path = txtDir.Text;
+            //string dPath = Path.Combine(path, "PDF");
+            var dirInfo = new DirectoryInfo(path);
+            var lDir = dirInfo.GetDirectories("*", SearchOption.TopDirectoryOnly).ToList();
+            var files = new List<string>();
+            foreach (var dir in lDir)
+            {
+                var lFiles = Directory.GetFiles(dir.FullName, "*.*", SearchOption.TopDirectoryOnly);
+                foreach (var lfile in lFiles)
+                {
+                    files.Add(lfile);
+                }
+            }
+            var counter = 0;
+
+            // var files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                try
+                {
+                    counter++;
+                    var pct = Convert.ToDouble(Convert.ToDouble(counter) / Convert.ToDouble(files.Count())) * 100;
+                    lblPb.Text = Path.GetFileName(file);
+
+                    var dVal = progressBar1.Value + pct;
+                    var val = Convert.ToInt32(dVal);
+                    if (val > 100)
+                    {
+                        val = 100;
+                    }
+                    progressBar1.Value = val;
+                    txtDetails.Text += @"Converting file : " + file + Environment.NewLine;
+                    txtDetails.SelectionStart = txtDetails.Text.Length;
+                    txtDetails.ScrollToCaret();
+                    System.Windows.Forms.Application.DoEvents();
+                    var ext = Path.GetExtension(file);
+                    var fn = Path.GetFileNameWithoutExtension(file);
+                    if (ext.ToLower() == ".tiff" || ext.ToLower() == ".tif" || ext.ToLower() == ".jpg" || ext.ToLower() == ".jpeg")
+                    {
+                        PdfDocument doc = new PdfDocument();
+                        PdfSection section = doc.Sections.Add();
+                        PdfPageBase page = doc.Pages.Add();
+
+                        //Load a tiff image from system
+                        PdfImage image = PdfImage.FromFile(file);
+                        //Set image display location and size in PDF
+                        float widthFitRate = image.PhysicalDimension.Width / page.Canvas.ClientSize.Width;
+                        float heightFitRate = image.PhysicalDimension.Height / page.Canvas.ClientSize.Height;
+                        float fitRate = Math.Max(widthFitRate, heightFitRate);
+                        float fitWidth = image.PhysicalDimension.Width / fitRate;
+                        float fitHeight = image.PhysicalDimension.Height / fitRate;
+                        page.Canvas.DrawImage(image, 0, 30, fitWidth, fitHeight);
+
+                        //save and launch the file
+                        doc.SaveToFile(Path.Combine(Path.GetDirectoryName(file), fn + ".pdf"));
+                        doc.Close();
+                    }
+                    else if (ext.ToLower() == ".html" || ext.ToLower() == ".htm")
+                    {
+                        PdfDocument doc = new PdfDocument();
+
+                        PdfPageSettings setting = new PdfPageSettings();
+
+                        setting.Size = new SizeF(1000, 1000);
+                        setting.Margins = new PdfMargins(20);
+
+                        PdfHtmlLayoutFormat htmlLayoutFormat = new PdfHtmlLayoutFormat();
+                        htmlLayoutFormat.IsWaiting = true;
+
+                        Thread thread = new Thread(() =>
+                        { doc.LoadFromFile(file, FileFormat.HTML); });
+                        thread.SetApartmentState(ApartmentState.STA);
+                        thread.Start();
+                        thread.Join();
+                        //Save pdf file.
+                        doc.SaveToFile(Path.Combine(Path.GetDirectoryName(file), fn + ".pdf"));
+                        doc.Close();
+                    }
+                    else if (ext.ToLower() == ".doc" || ext.ToLower() == ".docx")
+                    {
+                        var appWord = new Microsoft.Office.Interop.Word.Application();
+                        appWord.Visible = false;
+                        var wordDocument = appWord.Documents.Open(file);
+                        wordDocument.ExportAsFixedFormat(Path.Combine(Path.GetDirectoryName(file), fn + ".pdf"), WdExportFormat.wdExportFormatPDF);
+                        wordDocument.Close();
+                        appWord.Quit();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    SimpleLog.Log(ex);
+                    txtDetails.Text += Environment.NewLine;
+                    txtDetails.Text += @"ERROR Converting file - " + file + "----" + ex.Message + Environment.NewLine;
+                    txtDetails.Text += Environment.NewLine;
+                }
+
+            }
+            progressBar1.Value = 100;
+            System.Windows.Forms.Application.DoEvents();
+            MessageBox.Show("הפעולה הסתיימה בהצלחה");
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
